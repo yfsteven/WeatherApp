678149476c43300681c40af96dc9bd2865007c90
yfsteven <yaninquiry@gmail.com>
2024-01-30 17:11:55 -0500

    Customized design, correctly display API data, and other fixes

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ for(let i = 0; i < listItems.length; i++) {
     listWrapper.appendChild(h2);
 }
 
-const rname = document.getElementById('name');
-const rregion = document.getElementById('region');
-const rcountry = document.getElementById('country');
-const rlat = document.getElementById('lat');
-const rlon = document.getElementById('lon');
-const rlocaltime = document.getElementById('localtime');
-const rtemp_c = document.getElementById('temp_c');
-const rtemp_f = document.getElementById('temp_f');
-
 const div = document.createElement('div');
 div.id = 'condition-image--wrapper';
 const h2 = document.createElement('h2');
@@ -28,11 +19,20 @@ img.id = 'image';
 div.append(img);
 listWrapper.appendChild(div);
 
+const rname = document.getElementById('name');
+const rregion = document.getElementById('region');
+const rcountry = document.getElementById('country');
+const rlat = document.getElementById('lat');
+const rlon = document.getElementById('lon');
+const rlocaltime = document.getElementById('localtime');
+const rtemp_c = document.getElementById('temp_c');
+const rtemp_f = document.getElementById('temp_f');
+
 const rcondition = document.getElementById('condition')
 const rimage = document.getElementById('image');
 
 async function getWeather() {
-    const res = await fetch('https://api.weatherapi.com/v1/current.json?key=rooklyn', {mode: 'cors'});
+    const res = await fetch('https://api.weatherapi.com/v1/current.json?key=&q=Brooklyn', {mode: 'cors'});
     const weatherData = await res.json();
     const data = weatherData.location;
     const rcurrent = weatherData.current;
@@ -40,11 +40,13 @@ async function getWeather() {
     rname.textContent = data.name;
     rregion.textContent = data.region;
     rcountry.textContent = data.country;
-    rlat.textContent = data.lat + ' Latitude';
-    rlon.textContent = data.lon + ' Longitude';
+    rlat.textContent = data.lat + ' °Latitude';
+    rlon.textContent = data.lon + ' °Longitude';
+    rlocaltime.textContent = data.localtime;
+    rtemp_c.textContent = `${rcurrent.temp_c} °C`;
+    rtemp_f.textContent = `${rcurrent.temp_f} °F`;
     rcondition.textContent = routside.text;
     rimage.src = routside.icon;
-    console.log(data)
 }
 getWeather();
 console.log(listWrapper.outerHTML);
